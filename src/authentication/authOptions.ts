b9178e20dc96812a59301b1b6e3d875f8f85b84e
Peter Skoda <peters2@kentico.com>
2019-06-10 16:01:07 +0200

    Added configuration for github pages

--- src/authentication/authOptions.ts
+++ src/authentication/authOptions.ts
@@ -4,14 +4,14 @@ import {
 } from 'auth0-js';
 
 export const authOptions: AuthOptions = {
-  domain: 'kentico-dev.eu.auth0.com',
-  clientID: 'NPIPF1KyuQ7W0pgfE50nms09aDUR4mKi',
+  domain: process.env.REACT_APP_AUTH_DOMAIN!,
+  clientID: process.env.REACT_APP_AUTH_CLIENT_ID!,
   audience: 'https://app.kenticocloud.com/',
-  redirectUri: 'http://localhost:3000/callback',
+  redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URL,
   responseType: 'token id_token',
   scope: 'openid',
 };
 
 export const logoutOptions: LogoutOptions = {
-  returnTo: 'http://localhost:3000/logout',
+  returnTo: process.env.REACT_APP_AUTH_LOGOUT_RETURN_TO,
 };
