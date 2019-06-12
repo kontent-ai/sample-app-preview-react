90312be2a4d364fcf171df8d63595cb4fe6519e1
Peter Skoda <peters2@kentico.com>
2019-06-12 14:32:17 +0200

    Updates

--- precache-manifest.ea8262a8af7dc0f6e90016de76c8e8b2.js
+++ precache-manifest.ea8262a8af7dc0f6e90016de76c8e8b2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "59abfa725eb89aec85db0e91dcc8a24d",
+    "revision": "1a68794b691384c8ed5eb47a36760c3b",
     "url": "/cloud-sample-app-preview-react/index.html"
   },
   {
-    "revision": "6a0f27380a71c1982e2e",
+    "revision": "ed71cb22e541e77862c5",
     "url": "/cloud-sample-app-preview-react/static/css/main.da7bb949.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cloud-sample-app-preview-react/static/js/2.93156482.chunk.js"
   },
   {
-    "revision": "6a0f27380a71c1982e2e",
-    "url": "/cloud-sample-app-preview-react/static/js/main.4fdc3917.chunk.js"
+    "revision": "ed71cb22e541e77862c5",
+    "url": "/cloud-sample-app-preview-react/static/js/main.a7d2b4e1.chunk.js"
   },
   {
     "revision": "ddf72859d9df924f9cd3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cloud-sample-app-preview-react/precache-manifest.01cd587a46c2b6e06fc09fa88e1fbaf0.js"
+  "/cloud-sample-app-preview-react/precache-manifest.ea8262a8af7dc0f6e90016de76c8e8b2.js"
 );
 
 self.addEventListener('message', (event) => {
