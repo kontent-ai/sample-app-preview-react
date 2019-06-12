daf0fa1097ea33da3f43e28ea4072ee8688df02f
Peter Skoda <peters2@kentico.com>
2019-06-12 10:28:43 +0200

    Fixed example url in error page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -12,7 +12,7 @@ interface IErrorPageProps {
 
 const MissingProjectIdErrorPageContent: React.FunctionComponent = () => (
   <>
-    <p>Didn't you forget to provide Project Id in url? E.g. <i>https://kentico.github.io/cloud-preview-sample-app/your_project_id</i></p>
+    <p>Didn't you forget to provide Project Id in url? E.g. <i>https://kentico.github.io/cloud-sample-app-preview-react/your_project_id</i></p>
     <p>Default: <a href={`${RootRoute}cf6c9bb2-6d7e-00f8-a84a-b8042d1209c6`}>cf6c9bb2-6d7e-00f8-a84a-b8042d1209c6</a></p>
   </>
 );
