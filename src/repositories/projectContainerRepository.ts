b63bae19c918b31c16717db6442f00e15c4b30a0
Ivan Kiral <kiralivan@gmail.com>
2024-06-04 13:17:22 +0200

    update from ajax to fetch

--- src/repositories/projectContainerRepository.ts
+++ src/repositories/projectContainerRepository.ts
@@ -1,19 +1,25 @@
-import { createAjaxWithCredentials } from '../utils/ajax';
-import {
-  createRestProvider,
-  IRequestContext,
-} from '../utils/restProvider';
-
-const restProvider = createRestProvider(createAjaxWithCredentials());
+import { RequestContext, get } from '../utils/fetch';
 
 export interface ProjectContainer {
   projectContainerId: string;
 }
 
-export const getProjectContainerForEnvironment = (authToken: string, environmentId: string): Promise<ProjectContainer> => {
-  const requestContext: IRequestContext = {
+export const getProjectContainerForEnvironment = (
+  authToken: string,
+  environmentId: string,
+): Promise<ProjectContainer | null> => {
+  const requestContext: RequestContext = {
     authToken: authToken,
   };
   const url = `${process.env.REACT_APP_KONTENT_URL}/api/project-management/${environmentId}`;
-  return restProvider.get(url, requestContext);
+
+  return get(url, requestContext)
+    .then(async (res) => {
+      if(res.ok) {
+        return await res.json() as ProjectContainer
+      }
+      
+      console.error((await res.json()).description);
+      return null;
+    });
 };
